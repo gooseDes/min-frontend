c710cb07fea03ac84452b8682da03a75208ba794
gooseDes <sasaprst32@gmail.com>
2025-08-23 19:02:48 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    proxy: {
+      '/socket.io': {
+        target: 'https://server.msg-min.xyz',
+        ws: true,
+        changeOrigin: true
+      }
+    },
   },
 });
