6618cad19bec7d4f9dc786cbf31e909f346c12c9
gooseDes <sasaprst32@gmail.com>
2025-08-28 20:29:46 +0300

    trying to fix smth

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,13 @@ import { urlBase64ToUint8Array } from './push';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 if ("serviceWorker" in navigator && "PushManager" in window) {
     try {
+        navigator.serviceWorker.getRegistrations().then(registrations => {
+            for (let reg of registrations) {
+                reg.unregister().then(success => {
+                console.log("SW removed:", success);
+                });
+            }
+        });
         const reg = await navigator.serviceWorker.register("/sw.js", { type: 'module' });
         console.log("Service Worker registered");
 
