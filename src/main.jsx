16eeb4d804db6c6e65c09ca4bf57372bba48a3bc
gooseDes <sasaprst32@gmail.com>
2025-08-28 19:31:00 +0300

    better push messages

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './i18n';
+import { urlBase64ToUint8Array } from './push';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+if ("serviceWorker" in navigator && "PushManager" in window) {
+    try {
+        const reg = await navigator.serviceWorker.register("/sw.js", { type: 'module' });
+        console.log("Service Worker registered");
+
+        const subscription = await reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(
+                import.meta.env.VITE_VAPID_PUBLIC
+            ),
+        });
+    } catch (err) {
+        console.log("Service is not registered:", err);
+    }
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
--- src/push.js
+++ src/push.js
@@ -3,28 +3,28 @@ import { address } from "./wsClient";
 export async function subscribeUser() {
     if ("serviceWorker" in navigator && "PushManager" in window) {
         try {
-        const reg = await navigator.serviceWorker.register("/sw.js");
-        console.log("Service Worker registered");
+            const reg = await navigator.serviceWorker.register("/sw.js", { type: 'module' });
+            console.log("Service Worker registered");
 
-        const subscription = await reg.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(
-                import.meta.env.VITE_VAPID_PUBLIC
-            ),
-        });
+            const subscription = await reg.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(
+                    import.meta.env.VITE_VAPID_PUBLIC
+                ),
+            });
 
-        console.log("Got subscription:", subscription);
+            console.log("Got subscription:", subscription);
 
-        await fetch(`${address}/subscribe`, {
-            method: "POST",
-            body: JSON.stringify({
-                subscription: subscription,
-                token: localStorage.getItem('token')
-            }),
-            headers: { "Content-Type": "application/json" },
-        });
+            await fetch(`${address}/subscribe`, {
+                method: "POST",
+                body: JSON.stringify({
+                    subscription: subscription,
+                    token: localStorage.getItem('token')
+                }),
+                headers: { "Content-Type": "application/json" },
+            });
 
-        console.log("Subscription sent to server ✅");
+            console.log("Subscription sent to server ✅");
         } catch (err) {
             console.error("Push subscription error:", err);
         }
@@ -33,7 +33,7 @@ export async function subscribeUser() {
     }
 }
 
-function urlBase64ToUint8Array(base64String) {
+export function urlBase64ToUint8Array(base64String) {
     const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
     const rawData = atob(base64);
