c264ea9bb140ffdc5ba9042af19fd904b4d6e8d8
gooseDes <sasaprst32@gmail.com>
2025-09-05 22:11:49 +0300

    now opening for sure

--- src/gui/message.jsx
+++ src/gui/message.jsx
@@ -2,63 +2,35 @@ import { useSearchParams } from 'react-router-dom';
 import './message.css'
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { useState } from 'react';
 
 function Message({ text = 'message', author = 'author', type = 'left', src = '/logo512.png', connected = false }) {
     const [searchParams, setSearchParams] = useSearchParams();
-    const [selectedImg, setSelectedImg] = useState(null);
 
     function openAuthorProfile() {
-        searchParams.set('profile', author === 'You' ? localStorage.getItem('username') : author); 
+        searchParams.set('profile', author == 'You' ? localStorage.getItem('username') : author); 
         setSearchParams(searchParams);
     }
 
-    function openImage(url) {
-        setSelectedImg(url);
-    }
-
-    function closeImage() {
-        setSelectedImg(null);
-    }
-
     return (
-        <>
-            <div className={`MessageDiv ${type}`}>
-                <img
-                    className={`MessageAvatar${connected ? ' connected' : ''}`}
-                    src={src}
-                    onError={(e) => (e.currentTarget.src = '/logo512.png')}
-                    draggable="false"
-                    onClick={openAuthorProfile}
-                />
-                <div className={`TextDiv ${type}`}>
-                    <div className="Author" onClick={openAuthorProfile}>
-                        {author}
-                    </div>
-                    <Markdown
-                        remarkPlugins={[remarkGfm]}
-                        components={{
-                            img: ({ node, ...props }) => (
-                                <img
-                                    {...props}
-                                    className="MessageImage"
-                                    onClick={() => openImage(props.src)}
-                                />
-                            ),
-                        }}
-                    >
-                        {text.replaceAll('\n', '  \n')}
-                    </Markdown>
-                </div>
+        <div className={`MessageDiv ${type}`}>
+            <img className={`MessageAvatar${connected ? ' connected' : ''}`} src={src} onError={(e) => e.currentTarget.src='/logo512.png'} draggable='false' onClick={openAuthorProfile} />
+            <div className={`TextDiv ${type}`}>
+                <div className='Author' onClick={openAuthorProfile}>{author}</div>
+                <Markdown
+                    remarkPlugins={[remarkGfm]}
+                    components={{
+                        img: ({ node, ...props }) => (
+                        <a href={props.src} target="_blank" rel="noopener noreferrer">
+                            <img {...props} style={{ cursor: "pointer" }} />
+                        </a>
+                        ),
+                    }}
+                >
+                    {text.replaceAll('\n', '  \n')}
+                </Markdown>
             </div>
-
-            {selectedImg && (
-                <div className="ImageOverlay" onClick={closeImage}>
-                    <img src={selectedImg} className="ImageOverlayContent" />
-                </div>
-            )}
-        </>
-    );
+        </div>
+    )
 }
 
-export default Message;
+export default Message
