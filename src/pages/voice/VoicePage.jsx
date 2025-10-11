efa596c3c669fc2b4353ccc81d3d3215d1f648ad
gooseDes <sasaprst32@gmail.com>
2025-10-11 18:13:39 +0300

    voice chat(kinda works)

--- src/pages/voice/VoicePage.jsx
+++ src/pages/voice/VoicePage.jsx
@@ -1,63 +1,84 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getSocket } from "../../wsClient";
 
 function VoicePage() {
+    const [inited, setInited] = useState(false);
+
     useEffect(() => {
+        if (inited) return;
+        setInited(true);
         const ws = getSocket();
-        const pc = new RTCPeerConnection({
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-        });
 
-        const remoteAudio = document.getElementById("remoteAudio");
+        ws.on("turnUrls", (data) => {
+            navigator.mediaDevices
+                .getUserMedia({ audio: true })
+                .then((stream) => {
+                    const pc = new RTCPeerConnection({
+                        iceServers: data.urls,
+                    });
 
-        pc.ontrack = (event) => {
-            remoteAudio.srcObject = event.streams[0];
-        };
+                    stream.getTracks().forEach((track) => pc.addTrack(track, stream));
 
-        pc.onicecandidate = (event) => {
-            if (event.candidate) {
-                ws.emit("voiceAction", { chat: 1, action: "candidate", candidate: event.candidate });
-            }
-        };
+                    const remoteAudio = document.getElementById("remoteAudio");
 
-        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-            stream.getTracks().forEach((track) => pc.addTrack(track, stream));
-        });
+                    pc.ontrack = (event) => {
+                        if (remoteAudio.srcObject !== event.streams[0]) {
+                            remoteAudio.srcObject = event.streams[0];
+                        }
+                    };
 
-        ws.on("voiceAction", async (data) => {
-            if (typeof data === "string") data = JSON.parse(data);
-            console.log("voiceAction:", data);
+                    pc.onicecandidate = (event) => {
+                        if (event.candidate) {
+                            ws.emit("voiceAction", { chat: 1, action: "candidate", candidate: event.candidate });
+                        }
+                    };
 
-            if (data.action === "offer") {
-                await pc.setRemoteDescription(data.offer);
-                const answer = await pc.createAnswer();
-                await pc.setLocalDescription(answer);
-                ws.emit("voiceAction", { chat: 1, action: "answer", answer });
-            } else if (data.action === "answer") {
-                await pc.setRemoteDescription(data.answer);
-            } else if (data.action === "candidate") {
-                try {
-                    await pc.addIceCandidate(data.candidate);
-                } catch (err) {
-                    console.error("Error adding ICE candidate:", err);
-                }
-            }
-        });
+                    ws.on("voiceAction", async (data) => {
+                        if (typeof data === "string") data = JSON.parse(data);
+                        console.log("voiceAction:", data);
 
-        ws.emit("joinVoice", { chat: 1 });
+                        if (data.action === "offer") {
+                            await pc.setRemoteDescription(data.offer);
+                            const answer = await pc.createAnswer();
+                            await pc.setLocalDescription(answer);
+                            ws.emit("voiceAction", { chat: 1, action: "answer", answer });
+                        } else if (data.action === "answer") {
+                            await pc.setRemoteDescription(data.answer);
+                        } else if (data.action === "candidate") {
+                            try {
+                                await pc.addIceCandidate(data.candidate);
+                            } catch (err) {
+                                console.error("Error adding ICE candidate:", err);
+                            }
+                        }
+                    });
 
-        ws.on("joinedVoice", (data) => {
-            if (data.role === "offer") {
-                ws.on("userJoined", async (joinedData) => {
-                    console.log("userJoined:", joinedData);
-                    if (joinedData.user !== ws.id) {
-                        const offer = await pc.createOffer();
-                        await pc.setLocalDescription(offer);
-                        ws.emit("voiceAction", { chat: 1, action: "offer", offer });
-                    }
+                    ws.emit("joinVoice", { chat: 1 });
+
+                    ws.on("joinedVoice", (data) => {
+                        console.log("joinedVoice:", data);
+                        if (data.role === "offer") {
+                            ws.on("userJoined", async (joinedData) => {
+                                console.log("userJoined:", joinedData);
+                                if (joinedData.user !== ws.id) {
+                                    const offer = await pc.createOffer();
+                                    await pc.setLocalDescription(offer);
+                                    ws.emit("voiceAction", { chat: 1, action: "offer", offer });
+                                }
+                            });
+                        }
+                    });
+                })
+                .catch((error) => {
+                    console.error("Error getting user media:", error);
                 });
-            }
         });
+
+        ws.on("error", (data) => {
+            console.error(data);
+        });
+
+        ws.emit("getTurnUrls", { chat: 1 });
     }, []);
 
     return (
