63699e2c6fa04171a271b67e938c6e79cbf272ce
gooseDes <sasaprst32@gmail.com>
2026-06-03 21:12:26 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,30 @@
+import { address } from "../src/wsClient.js";
+
+self.addEventListener("push", (event) => {
+    console.log("Push received:", event);
+    let data;
+    try {
+        data = event.data.json();
+    } catch {
+        console.log(event.data);
+        return;
+    }
+
+    event.waitUntil(
+        self.registration.showNotification(data.chat, {
+            body: data.message,
+            icon: `${address}/avatars/${data.author}.webp`,
+            badge: `${address}/avatars/${data.author}.webp`,
+            tag: "message",
+            vibration: [200, 100, 200, 100, 400],
+        }),
+    );
+});
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+});
